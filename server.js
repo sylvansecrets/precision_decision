3491b5523818acf6426ecee79988b9db58ff4691
cody j <codywj777@gmail.com>
2016-11-24 23:39:54 +0000

    commiting minor changes to server (negligible)

--- server.js
+++ server.js
@@ -37,20 +37,19 @@ app.get('/', (req, res) => {
   // Sends initial poll data to db and loads preview
 app.post('/polls/:id/preview', (req, res) => {
   // Adds poll to database, leaving is_sent = false
+
+  // #################################
+  //   This is where we use add_poll
+  // #################################
+
   // Creates all necessary unique IDs
   // redirects to get /polls/id/preview
 });
 
-
-  // Gets preview of poll with footer
+// +------------------------------------------+
+// |     Gets preview of poll with footer     |
+// +------------------------------------------+
 app.get('/polls/:id/preview', (req, res) => {
-  // retrieves poll and appends to DOM
-  // and footer with two buttons: Send || Edit
-});
-
-
-  // Gets edit page
-app.get('/polls/:id/edit', (req, res) => {
   const pollId = req.params.id
   function getQuestionsByPollId(pollId) {
     return knex.select('question_text')
@@ -58,7 +57,42 @@ app.get('/polls/:id/edit', (req, res) => {
         .join('options', 'polls.id', '=', 'options.poll_id')
         .where('polls.id', pollId)
         .orderBy('options')
+  }
+  function getEmailsByPollId(pollId) {
+    return knex.select('email')
+        .from('polls')
+        .join('users', 'polls.id', '=', 'users.poll_id')
+        .where('polls.id', pollId)
+        .orderBy('email')
+  }
+  questionPromise = getQuestionsByPollId(pollId);
+  emailPromise = getEmailsByPollId(pollId);
+  Promise.all([questionPromise, emailPromise])
+  .then((resolutions) => {
+    console.log(resolutions);
+    res.render('pages/polls_preview', {
+      questions: resolutions[0],
+      emails: resolutions[1]
+    })
+  })
+  .catch((err)=> {
+    console.log("That poll does not exist.", err);
+    res.status(404).res.send('That poll does not exist.');
+  })
+});
 
+// +------------------------------------------+
+// |            Gets Edit Page                |
+// +------------------------------------------+
+app.get('/polls/:id/edit', (req, res) => {
+  const pollId = req.params.id
+
+  function getQuestionsByPollId(pollId) {
+    return knex.select('question_text')
+        .from('polls')
+        .join('options', 'polls.id', '=', 'options.poll_id')
+        .where('polls.id', pollId)
+        .orderBy('options')
   }
   function getEmailsByPollId(pollId) {
     return knex.select('email')
@@ -81,9 +115,6 @@ app.get('/polls/:id/edit', (req, res) => {
     console.log("That poll does not exist.", err);
     res.status(404).res.send('That poll does not exist.');
   })
-
-  // if is_sent = false
-  // Retrieves poll and appends to DOM with /polls/new skeleton
 
 });
 
