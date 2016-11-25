1422c896f919389bf7116ed8a31d59d4307c8e31
cody j <codywj777@gmail.com>
2016-11-25 02:58:49 +0000

    made some adjustments to the server.js

--- server.js
+++ server.js
@@ -11,8 +11,8 @@ const bodyParser  = require('body-parser');
 const PORT        = process.env.PORT || 8080;
 const config      = require('./config');
 const knex        = require('knex')(config);
-const commitPollToDb = require('./server/database_api/db_add_poll');
-const commitEditsToDb = require('./server/database_api/db_edit_poll');
+// const commitPollToDb = require('./server/database_api/db_add_poll');
+// const commitEditsToDb = require('./server/database_api/db_edit_poll');
 
 app.use(express.static("public"));
 //app.use(express.static("views/partials"));
@@ -25,9 +25,7 @@ app.set('view engine', 'ejs');
 
   // Landing Page // Create Poll Page
 app.get('/', (req, res) => {
-  res.render('pages/index', {
-    edit: false;
-  });
+  res.render('pages/index');
 });
 
 
@@ -41,39 +39,65 @@ app.get('/', (req, res) => {
   // Sends initial poll data to db and loads preview
 app.post('/polls/:id/preview', (req, res) => {
   // Adds poll to database, leaving is_sent = false
-  const poll
-  commitPollToDb
+
+
   // Creates all necessary unique IDs
   // redirects to get /polls/id/preview
 });
+
+
+
+
+
+
+
 
 // +------------------------------------------+
 // |     Gets preview of poll with footer     |
 // +------------------------------------------+
 app.get('/polls/:id/preview', (req, res) => {
   const pollId = req.params.id
+  function getAdminBool(pollId) {
+    return knex.select('admin')
+        .from('users')
+        .where('poll_id', pollId);
+  }
+  function getIsSentBool(pollId) {
+    return knex.select('isSent')
+        .from('polls')
+        .where('id', pollId);
+  }
   function getQuestionsByPollId(pollId) {
     return knex.select('question_text')
         .from('polls')
         .join('options', 'polls.id', '=', 'options.poll_id')
         .where('polls.id', pollId)
-        .orderBy('options')
+        .orderBy('options');
   }
   function getEmailsByPollId(pollId) {
     return knex.select('email')
         .from('polls')
         .join('users', 'polls.id', '=', 'users.poll_id')
         .where('polls.id', pollId)
-        .orderBy('email')
+        .orderBy('email');
   }
   questionPromise = getQuestionsByPollId(pollId);
   emailPromise = getEmailsByPollId(pollId);
-  Promise.all([questionPromise, emailPromise])
+  isSentBoolPromise = getIsSentBool(pollId);
+  adminBool = getAdminBool(pollId);
+  Promise.all([     questionPromise,
+                    emailPromise,
+                    isSentBoolPromise,
+                    adminBool
+              ])
   .then((resolutions) => {
     console.log(resolutions);
+
     res.render('pages/polls_preview', {
       questions: resolutions[0],
-      emails: resolutions[1]
+      emails: resolutions[1],
+      isSent: resolutions[2],
+      admin: resolutions[3]
     })
   })
   .catch((err)=> {
@@ -81,6 +105,11 @@ app.get('/polls/:id/preview', (req, res) => {
     res.status(404).res.send('That poll does not exist.');
   })
 });
+
+
+
+
+
 
 // +------------------------------------------+
 // |            Gets Edit Page                |
@@ -107,10 +136,9 @@ app.get('/polls/:id/edit', (req, res) => {
   Promise.all([questionPromise, emailPromise])
   .then((resolutions) => {
     console.log(resolutions);
-    res.render('pages/polls_edit', {
+    res.render('pages/polls', {
       questions: resolutions[0],
-      emails: resolutions[1],
-      edit: true;
+      emails: resolutions[1]
     })
   })
   .catch((err)=> {
