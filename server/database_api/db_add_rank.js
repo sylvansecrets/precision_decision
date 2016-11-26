134b0eba0f87d4d90d5e59f36aaea9545d3c44bd
Richard Zhang <lapislacrima@gmail.com>
2016-11-26 04:58:35 +0000

    added a function to collapse the output into the format {unique_string: {option:rank}}

--- server/database_api/db_add_rank.js
+++ server/database_api/db_add_rank.js
@@ -11,33 +11,6 @@ const Promise = require('bluebird');
 // const options_rank = {2: 4, 1: 2};
 // const st = 'x711wtbpk9mk1gh5'
 
-// function addRank(unique_string, options_rank){
-//   knex.transaction(function(t){
-//     knex('users')
-//       .transacting(t)
-//       .first('id')
-//       .where('unique_string', '=', unique_string)
-//     .then(function(user_id){
-//       user_id = user_id['id']
-//       let ranking_promise = [];
-//       for (let option in options_rank){
-//         ranking_promise.push(injectRank(user_id, option, options_rank[option], t));
-//       }
-//       return Promise.all(ranking_promise);
-//     })
-//     .then(t.commit)
-//     .catch((err)=>{
-//       t.rollback();
-//       console.log("transaction failed, \n", err)
-//     })
-//   })
-//   .then(function(){
-//     console.log("Success, added rank");
-//     process.exit();
-//   })
-//   .catch(function(err){console.log("Failure", err)})
-// }
-
 function addRank(unique_string, options_rank){
   return knex.transaction(function(t){
     let ranking_promise = []
@@ -67,4 +40,4 @@ function injectRank(unique_string, option_id, rank, transact){
 
 module.exports = {addRank: addRank}
 
-addRank('1912uf0h9z31a9f6', {2: 4, 1: 2})
+// addRank('1912uf0h9z31a9f6', {2: 4, 1: 2})
